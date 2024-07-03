01908e3ceed247aa2719a607ff7e41428a55e85b
Omar Hernandez Salmeron <ohsalmeron@gmail.com>
2024-07-03 13:21:44 -0600

    Identity script update
    Prompting for number of identities

--- register_identities.sh
+++ register_identities.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-# Loop to create 64 player identities
-for i in {1..64}; do
+# Prompt user for number of player identities
+echo -n "Enter the number of player identities to create: "
+read num_players
+
+# Loop to create player identities
+for ((i=1; i<=$num_players; i++)); do
   dfx identity new player$i
 done
 
-echo "Created 64 player identities."
+echo "Created $num_players player identities."
